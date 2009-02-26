95af140a300ac1f91edadfd32ffefd046c4df358
Kevin Thompson <mrunleaded@gmail.com>
2009-02-25 21:32:13 -0800

    Moving Gitty to Gitty.Api to break into a submodule

---
+++ IDiffFilesFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gitty
+{
+    public enum DiffMode
+    {
+        Add = 'A',
+        Copy = 'C',
+        Delete = 'D',
+        Modification = 'M',
+        Rename = 'R',
+        TypeChange = 'T',
+        Unmerged = 'U',
+        Unknown = 'X',
+    }
+
+    public interface IDiffFilesFile: IWorkingDirectoryFile, IIndexFile
+    {
+        DiffMode Type{ get; }
+    }
+}
---
+++ IDiffIndexFile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gitty
+{
+    public interface IDiffIndexFile : IIndexFile, IRepositoryFile
+    {
+        DiffMode Type { get; }
+    }
+}
